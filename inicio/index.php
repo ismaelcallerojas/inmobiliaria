d0ff6e37a6938ac98a350da991b976de4afbe182
alex <alex.averanga@uab.edu.bo>
2021-10-25 22:07:54 -0400

    Plantilla para el proyecto
    Creamos la plantilla que se usara en el reesto del proyecto

---
+++ inicio/index.php
@@ -0,0 +1,15 @@
+<?php include '../extend/header.php'; ?>
+    <div class="row">
+        <div class="col s12">
+            <div class="card">
+                <div class="card-content">
+                    <span class="card-title">Títulos</span>
+                    <p>Contenido</p>
+                </div>
+            </div>
+        </div>
+    </div>
+<?php include '../extend/scripts.php'; ?>
+    </body>
+    
+</html>
